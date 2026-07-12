a77ab88ea4d21ac4612431a26dc045d22c770289
Sanjana-Bhandari <kumarisanjana.work@gmail.com>
2026-07-12 21:26:55 +0530

    Fix admin login API URL

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -23,7 +23,7 @@ const AdminLogin = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/admin/login", {
+      const res = await fetch("https://personal-portfolio-r2t9.onrender.com/api/admin/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
